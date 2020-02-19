f799065d56b4c3485374131dcf4911ae2c091a8b
root <root@DESKTOP-QS49P76.localdomain>
2020-02-19 15:37:03 -0500

    Updates

---
+++ precache-manifest.5aadab7f958a60d308c5b35709cef80c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72d1021b2bd2a872f2a4b1476694f16",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "474119a3fe38a028d3d0",
+    "url": "/myapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/myapp/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myapp/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474119a3fe38a028d3d0",
+    "url": "/myapp/static/js/main.0522c757.chunk.js"
+  },
+  {
+    "revision": "5e9208f8bdc4936b5d0e",
+    "url": "/myapp/static/js/runtime-main.093b8167.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
